0000d494c29ce12bcf123da317a1ec29509a47f0
mikkaelll <53883518+mikkaelll@users.noreply.github.com>
2019-09-02 21:46:22 -0300

    Add files via upload

---
+++ TestaLoop.java
@@ -0,0 +1,14 @@
+
+public class TestaLoop {
+
+	public static void main(String[] args) {
+		int numero = 1;
+		while (numero<=10){
+		System.out.println(numero+" Bom dia ");
+		numero++;
+
+	}
+
+}
+
+}
